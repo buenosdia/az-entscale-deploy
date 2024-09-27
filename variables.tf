b91c0a0b10f949b471cd0d0b21bd2ba97a2c1e98
Eric <ecruz@geosolinc.com>
2024-09-27 10:15:33 -0400

    e4sdf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# Use variables to customize the deployment
+
+variable "root_id" {
+  type    = string
+  default = "someid"
+}
+
+variable "root_name" {
+  type    = string
+  default = "SomeName"
+}
